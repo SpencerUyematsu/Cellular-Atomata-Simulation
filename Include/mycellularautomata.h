46d94f7ba69c4b1f35fbe8ba6ce2d390d48fa7f2
Michael Soto <mikesoto2121@gmail.com>
2023-12-09 18:20:44 -0500

    conditional transition rule/majority rule neighborhood update get cell methods

--- Include/mycellularautomata.h
+++ Include/mycellularautomata.h
@@ -8,8 +8,8 @@
 using namespace std;
 
 struct cell{
-    int type;
-    int steps_passed;
+    int type; // state of cell
+    int steps_passed; // steps passed at current type
 };
 
 class cellular_automata{
@@ -20,7 +20,9 @@ class cellular_automata{
         int setup_states(int num_states, vector<double> state_probabilities);
         int setup_prob(vector<double> &state_probabilities);
         int print_CA_status();
-        int setup_neighborhood(int row, int column, std::string neighborhood_law);
+        int setup_neighborhood(std::string neighborhood_law);
+        std::vector<cell> get_neighborhood(int row, int column);
+        cell get_cell(int row, int col);
 
     private:
         cell **cellular_matrix;
@@ -28,7 +30,7 @@ class cellular_automata{
         int width_;
         int num_states_;
         std::string neighborhood_law_;
-        std::vector<cell> neighbors_;
+        std::vector<cell> neighborhood_;
 
 };
 
@@ -38,6 +40,7 @@ class cellular_automata{
 #define ERR_INC_PROBABILITY -3
 #define ERR_NO_SETUP -4
 #define NEIGHBORHOOD_INVALID_TYPE = - 5
+#define ERR_NEIGHBORHOOD_NOT_SET = -6
 
 class simulate{
     public:
@@ -46,6 +49,8 @@ class simulate{
         int setup_simulation(cellular_automata &CA);
         int step();
         int print_sim_status();
+        int straight_conditional_transition_rule(int row, int column, int initial_type, int final_type, int num_steps);
+        int conditional_transition_rule_on_neighbor(int row, int column, int initial_type, int neighbor_type);
 
     private:
         vector<cellular_automata> CA_record;
--- Source/Datatypes/simulation.cpp
+++ Source/Datatypes/simulation.cpp
@@ -5,3 +5,49 @@ simulate::simulate(){};
 simulate::~simulate(){};
 
 int simulate::setup_simulation(cellular_automata &CA);
+
+
+
+int stimulate::straight_conditional_transition_rule(int row, int column, int initial_type, int final_type, int num_steps) {
+    
+    if (CA.get_cell(row, column).type == initial_type && CA.get_cell(row, column).steps_passed >= num_steps) {
+        
+        CA.get_cell(row, column).type = final_type; // change cell to final type
+        CA.get_cell(row, column).steps_passed = 0; // reset steps passed
+    }
+
+    return NO_ERROR;
+}
+
+
+int simulate::conditional_transition_rule_on_neighbor(int row, int column, int initial_type, int neighbor_type) {
+
+    if (CA.get_cell(row, column).type != intial_type) {
+    
+        return NO_ERROR:
+    
+    }
+   
+   
+   std::vector<cell> neighbors = CA.get_neighborhood(row, column); // get neighbors of cell
+
+   for (size_t i = 0; i < neighbors.size(); i++) {
+        
+        if (neighbors[i].type == neighbor_type) {
+
+            CA.get_cell(row, column).type = neighbor_type;
+            CA.get_cell(row, column).steps_passed = 0;
+
+            break;
+        }
+
+       
+   }
+
+    return NO_ERROR;
+
+}
+
+
+
+
